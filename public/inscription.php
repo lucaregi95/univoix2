7475c0be7371a612f04adfac0710a42fa018e20f
FAJNZYN_Pr <P.FAJNZYN@lprs.fr>
2026-02-20 16:35:41 +0100

    resolution de bugs sur le fichier de connexion

--- public/inscription.php
+++ public/inscription.php
@@ -1,51 +1,73 @@
 <?php
 
-require_once "..\bdd\connexion.php";
+require_once "../bdd/connexion.php";
 
-if(isset($_POST["S'inscrire"])){
+if(isset($_POST["submit_btn"])){
     $nom = $_POST['nom'];
     $prenom = $_POST['prenom'];
-    $email = $_POST['email'];
-    $pseudo = $_POST['pseudo'];
-    $mdp = $_POST['mdp'];
-    $conf_mdp= $_POST['conf_mdp'];
-    $role = "inscrit";
-    $imp_sign = "1";
     $age = $_POST['age'];
+    $pseudo = $_POST['pseudo'];
+    $email = $_POST['email'];
+    $mot_de_passe = $_POST['mot_de_passe'];
+    $role = "user";
+    $importance_signalement = 1;
     $ville = $_POST['ville'];
 
+    if ($_POST['mot_de_passe']!==$_POST['conf_mdp']) {
+        echo "Les mots de passe ne correspondent pas";
+    }else{
+        $sql = "INSERT INTO inscrit (nom, prenom, age, pseudo, email, ville, mot_de_passe, role, importance_signalement) VALUES (:nom, :prenom, :age, :pseudo, :email, :ville, :mot_de_passe, :role, :importance_signalement)";
+        $query = $connexion->prepare($sql);
+        $query->execute(array(
+            "nom" => $nom,
+            "prenom" => $prenom,
+            "age" => $age,
+            "pseudo" => $pseudo,
+            "email" => $email,
+            "mot_de_passe" => $mot_de_passe,
+            "role" => $role,
+            "importance_signalement" => $importance_signalement,
+            "ville" => $ville
+        ));
 
-    $sql ="INSERT INTO etudiants (nom, prenom, age, pseudo,role, importance_signalement,mail,ville, mdp) VALUES (:nom, :prenom, :age, :pseudo,:role, :importance_signalement,:email,:ville, :mdp)";
-    $query = $connexion->prepare($sql);
-    $query->execute(array(
-            'nom' => $nom,
-        'prenom' => $prenom,
-        'age' => $age,
-        'pseudo' => $pseudo,
-        'role' => $role,
-        'importance_signalement' => $imp_sign,
-        'email' => $email,
-        'ville' => $ville,
-        'mdp' => $mdp
-    ));
+        header('Location: public/acceuil.php');
+        exit();
+    }
 }
 ?>
 
-<!DOCTYPE html>
-<form action="acceuil.php" method="post"><br>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Inscription</title>
+</head>
+<body>
+<form action="inscription.php" method="post">
     <label>Votre nom :</label>
-    <input name="nom" id="nom" type="text"><br>
+    <input name="nom" id="nom" type="text" required><br>
+
     <label>Votre prénom :</label>
-    <input name="prenom" id="prenom" type="text"><br>
-    <label>Votre age :</label>
-    <input name="age" id="age" type="number"><br>
+    <input name="prenom" id="prenom" type="text" required><br>
+
+    <label>Votre âge :</label>
+    <input name="age" id="age" type="number" required><br>
+
     <label>Votre pseudo :</label>
-    <input name="pseudo" id="pseudo" type="text"><br>
+    <input name="pseudo" id="pseudo" type="text" required><br>
+
+    <label>Votre ville :</label>
+    <input name="ville" id="ville" type="text" required><br>
+
     <label>Votre email :</label>
-    <input name="email" id="email" type="email"><br>
+    <input name="email" id="email" type="email" required><br>
+
     <label>Votre mot de passe :</label>
-    <input name="mdp" id="mdp" type="password"><br>
+    <input name="mot_de_passe" id="mot_de_passe" type="password" required><br>
+
     <label>Confirmation du mot de passe :</label>
-    <input name="conf_mdp" id="conf_mdp" type="password"><br>
-    <input type="submit" value="S'inscrire">
+    <input name="conf_mdp" id="conf_mdp" type="password" required><br>
+
+    <input type="submit" name="submit_btn" value="S'inscrire">
 </form>
+</body>
+</html>
