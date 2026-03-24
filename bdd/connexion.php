176badc9d556c45d3f85cda0fc4367b707d665ce
FAJNZYN_Pr <P.FAJNZYN@lprs.fr>
2026-03-24 16:00:57 +0100

    fix:modification des logins et mdp pour la connexion dans la base de donnée

--- bdd/connexion.php
+++ bdd/connexion.php
@@ -1,6 +1,7 @@
 <?php
 try {
-    $connexion = new PDO('mysql:host=localhost;dbname=univoix;charset=utf8', 'root','');
+    $connexion = new PDO('mysql:host=localhost;dbname=univoix;charset=utf8',
+        'univoix_admin','kg953iEPPn&DoHuE');
 } catch (PDOException $e) {
     echo ("Erreur :".$e->getMessage());
 }
